3cae0061c7eaecfb8b6ad09a0b1ccca3a401c9fc
Jacob Seymour <jacobwseymour@gmail.com>
2023-01-26 21:59:04 +0000

    added guidance card, table and modal form functions

--- assets/js/script.js
+++ assets/js/script.js
@@ -4,3 +4,13 @@ var update = function() {
     .innerHTML = moment().format('MMMM Do YYYY, h:mm:ss a');
 }
 setInterval(update, 1000);
+
+// DOM Elements
+var projectFormEl = $("#project-form");
+var activityNameEL = $(".activity-name-input");
+var activityTypeEl = $(".activity-type-input");
+var allocatedTimeEl = $(".allocated-time-input");
+var dueDateEl = $(".due-date-input");
+var closeFormEl = $(".closeBtn");
+var submitFormEl = $(".submitBtn");
+// var deleteActivity = $("")
